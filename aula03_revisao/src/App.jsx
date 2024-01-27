43b5ca9401da08a71cf410796d135babf3ac6c33
Kelson Ameida <kelson.victr@gmail.com>
2024-01-27 14:18:32 -0300

    Aula 04 - andamento

--- aula03_revisao/src/App.jsx
+++ aula03_revisao/src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-import HookMegaSena from './components/HookMegaSena'
+import Aluno from './components/Aluno'
 
 const App = () => {
+    const aluno1 = {
+      nome: "João",
+      email: "joao@gmail.com",
+      curso: "Sistemas para Internet"
+  }
+
+  const aluno2 = {
+      nome: "Maria",
+      email: "maria@gmail.com",
+      curso: "Curso Presencial Fullstack"
+  }
+
+const alunos = [aluno1, aluno2];
+
   return (
     <div>
-      <HookMegaSena />
+      {
+            alunos.map((aluno) => (
+                <Aluno nome={aluno.nome} email={aluno.email} curso={aluno.curso} />
+            ))
+        }
     </div>
   )
 }
