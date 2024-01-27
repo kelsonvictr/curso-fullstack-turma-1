1ca580dc0b40bbed7f33feeb3f42aa310824892f
Kelson Ameida <kelson.victr@gmail.com>
2024-01-27 15:18:07 -0300

    Aula 04 - andamento

---
+++ aula04/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Exercicio1Css from './components/Exercicio1Css'
+
+const App = () => {
+  return (
+    <>
+    <Exercicio1Css />
+    </>
+  )
+}
+
+export default App
---
+++ aula04/src/components/Exercicio1Css.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Exercicio1Css = () => {
+  return (
+    <>
+        <p>Essa turma é show!</p>
+        <img src='java.png' alt='Linguagem que mais contrata' />
+    </>
+  )
+}
+
+export default Exercicio1Css
