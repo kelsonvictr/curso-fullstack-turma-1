3dae2af5b244dc9eadb817a7ce9b2102d04448fa
Kelson Ameida <kelson.victr@gmail.com>
2024-04-13 15:49:14 -0300

    Aula 11 - ongoing

---
+++ api/src/main/java/br/com/gerenciadordeprodutos/api/User/controller/UserController.java
@@ -0,0 +1,41 @@
+package br.com.gerenciadordeprodutos.api.User.controller;
+
+import br.com.gerenciadordeprodutos.api.User.dtos.CreateUserRequest;
+import br.com.gerenciadordeprodutos.api.User.dtos.LoginUserRequest;
+import br.com.gerenciadordeprodutos.api.User.dtos.RecoveryJwtTokenDto;
+import br.com.gerenciadordeprodutos.api.User.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/login")
+    @PreAuthorize("isAnonymous()")
+    public ResponseEntity<RecoveryJwtTokenDto> authenticateUser
+            (@RequestBody LoginUserRequest loginUserRequest) {
+        RecoveryJwtTokenDto token = userService.authenticateUser(loginUserRequest);
+        return new ResponseEntity<>(token, HttpStatus.OK);
+    }
+
+    @PostMapping
+    @PreAuthorize("isAnonymous()")
+    public ResponseEntity<Void> createUser(@RequestBody CreateUserRequest createUserRequest) {
+        userService.createUser(createUserRequest);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/test")
+    @PreAuthorize("hasAuthority('ROLE_ADMINISTRATOR')")
+    public ResponseEntity<String> getAuthenticationTest() {
+        return new ResponseEntity<>("Success", HttpStatus.OK);
+    }
+}
