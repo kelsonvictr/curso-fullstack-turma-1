c50dc59193ee029d3a454985cb8620a25218cc18
Kelson Ameida <kelson.victr@gmail.com>
2024-04-13 16:34:53 -0300

    Aula 11 - cors

--- gerenciador_de_produtos_frontend/src/api/index.js
+++ gerenciador_de_produtos_frontend/src/api/index.js
@@ -1,24 +1,25 @@
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const api = axios.create({
   baseURL: 'http://localhost:8080/' // Ajuste conforme necessário
 });
 
 api.interceptors.request.use(config => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  }, error => Promise.reject(error));
-  
-  api.interceptors.response.use(response => response, error => {
-    if (error.response && error.response.status === 401) {
-      localStorage.removeItem('token'); // Remove o token do localStorage
-      // Redireciona para login
-      window.location.href = '/login';
-    }
-    return Promise.reject(error);
-  });
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, error => Promise.reject(error));
 
-export default instance
+api.interceptors.response.use(response => response, error => {
+  if (error.response && error.response.status === 401) {
+    localStorage.removeItem('token'); // Remove o token do localStorage
+    // Redireciona para login
+    window.location.href = '/login';
+  }
+  return Promise.reject(error);
+});
+
+export default api;
