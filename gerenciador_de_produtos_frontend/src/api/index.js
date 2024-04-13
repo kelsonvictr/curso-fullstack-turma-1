52c62e0c59cf6ee4e343304ba1a57d8f02d220a2
Kelson Ameida <kelson.victr@gmail.com>
2024-04-13 15:53:34 -0300

    Aula 11 - api/index.js

--- gerenciador_de_produtos_frontend/src/api/index.js
+++ gerenciador_de_produtos_frontend/src/api/index.js
@@ -1,7 +1,24 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const instance = axios.create({
-    baseURL: 'http://localhost:3000/'
-})
+const api = axios.create({
+  baseURL: 'http://localhost:8080/' // Ajuste conforme necessário
+});
+
+api.interceptors.request.use(config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  }, error => Promise.reject(error));
+  
+  api.interceptors.response.use(response => response, error => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('token'); // Remove o token do localStorage
+      // Redireciona para login
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  });
 
 export default instance
