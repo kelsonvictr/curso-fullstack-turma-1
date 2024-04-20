89ca18e75e3110978fe3a039645ecb37838eab1b
Kelson Ameida <kelson.victr@gmail.com>
2024-04-20 14:20:19 -0300

    Aula 12 - Andamento

--- api/src/main/java/br/com/gerenciadordeprodutos/api/Product/dtos/ProductRequest.java
+++ api/src/main/java/br/com/gerenciadordeprodutos/api/Product/dtos/ProductRequest.java
@@ -1,6 +1,8 @@
 package br.com.gerenciadordeprodutos.api.Product.dtos;
 
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 import java.math.BigDecimal;
@@ -9,8 +11,11 @@ import java.util.UUID;
 @Getter
 public class ProductRequest {
 
+    @NotBlank(message = "Nome é obrigatório")
     private String name;
 
+    @NotNull(message = "Preço é obrigatório")
+    @DecimalMin(value = "0.01", message = "O preço deve ser no mínimo R$0.01")
     private BigDecimal price;
 
     private UUID supplierId;
--- gerenciador_de_produtos_frontend/src/pages/Product/ProductForm.jsx
+++ gerenciador_de_produtos_frontend/src/pages/Product/ProductForm.jsx
@@ -7,6 +7,7 @@ const ProductForm = () => {
 
     const [product, setProduct] = useState({ name: '', price: '', supplierId: ''})
     const [suppliers, setSuppliers] = useState([])
+    const [errorMessage, setErrorMessage] = useState('')
     const navigate = useNavigate()
     const { id } = useParams()
 
@@ -15,14 +16,20 @@ const ProductForm = () => {
         .then(response => {
             setSuppliers(response.data)
         })
-        .catch(error => console.error('Erro ao buscar fornecedores', error))
+        .catch(error => {
+            console.error('Erro ao buscar fornecedores', error)
+            handleErrors(error)
+        } )
 
         if (id) {
             axios.get(`/products/${id}`)
             .then(response => {
                 setProduct(response.data)
             })
-            .catch(error => console.error('Erro ao buscar produto', error))
+            .catch(error => {
+                console.error('Erro ao buscar produto', error)
+                handleErrors(error)
+            })
         } else {
             setProduct({ name: '', price: '', supplierId: ''})
         }
@@ -43,12 +50,32 @@ const ProductForm = () => {
             alert(`Produto ${id ? 'atualizado' : 'adicionado'} com sucesso!`)
             navigate("/listar-produtos")
         })
-        .catch(error => console.error("Ocorreu um erro: ",error))
+        .catch(error => {
+            console.error("Ocorreu um erro: ",error)
+            handleErrors(error)
+        })
+    }
+
+    function handleErrors(error) {
+        if (error.response) {
+            if (error.response.status === 400) {
+                if (Array.isArray(error.response.data)) {
+                    setErrorMessage(error.response.data.join(', '))
+                } else {
+                    setErrorMessage(error.response.data.message || 'Ocorreu um erro desconhecido')
+                }
+            }
+        }
     }
 
   return (
     <div className="container mt-5">
         <h2>{id ? 'Editar Produto' : 'Adicionar Produto'}</h2>
+        {errorMessage && (
+            <div className="alert alert-danger" role="alert">
+                {errorMessage}
+            </div>
+        )}
         <form onSubmit={handleSubmit}>
             <div className="form-group">
                 <label htmlFor="name">Nome do Produto:</label>
